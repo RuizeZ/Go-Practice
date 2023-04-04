c89534d934e19e022134d83a699072e1de1f881c
Ruize Zhang <imruize@126.com>
2023-04-04 14:31:50 -0700

    hello world file

---
+++ helloworld/main.go
@@ -0,0 +1,9 @@
+// package with a name main is executable file, other names are reuseble file
+package main
+
+// fmt is a standard library. Import library before use it
+import "fmt"
+
+func main() {
+	fmt.Println("hi there!")
+}
